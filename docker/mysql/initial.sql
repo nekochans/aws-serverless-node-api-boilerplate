93c86443a79bc41f049f460e3c6b5b1433a717ab
keitakn <keita.koga.work@gmail.com>
2021-05-18 17:04:00 +0900

    :art: #5 Prismaのマイグレーションで必要になるので権限を追加

--- docker/mysql/initial.sql
+++ docker/mysql/initial.sql
@@ -3,3 +3,4 @@
 -- データベース 'serverless_test' への権限を付与
 CREATE DATABASE IF NOT EXISTS serverless_test CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 GRANT ALL ON serverless_test.* TO `serverless_test_user`@`%` IDENTIFIED BY 'Serverless_Test_User|8888';
+GRANT ALL ON *.* TO `serverless_test_user`@`%` IDENTIFIED BY 'Serverless_Test_User|8888';
