6283f932bcca2e70969d6dbc988e949dbca7ebdd
keitakn <keita.koga.work@gmail.com>
2021-05-18 16:46:53 +0900

    :sparkles: #5 開発用のMySQLコンテナを追加

---
+++ docker/mysql/initial.sql
@@ -0,0 +1,5 @@
+-- 'serverless_test' というデータベースを作成
+-- 'serverless_test_user' というユーザー名のユーザーを作成
+-- データベース 'serverless_test' への権限を付与
+CREATE DATABASE IF NOT EXISTS serverless_test CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+GRANT ALL ON serverless_test.* TO `serverless_test_user`@`%` IDENTIFIED BY 'Serverless_Test_User|8888';
