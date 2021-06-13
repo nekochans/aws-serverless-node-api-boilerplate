bdecac6dd944099babbe79221b41958e7a38e4d1
keitakn <keita.koga.work@gmail.com>
2021-06-13 15:07:15 +0900

    :green_heart: #21 擬似ttyが割り当てられてしまい、docker-compose execが正常実行出来ないので -Tを追加

--- migrate_down.sh
+++ migrate_down.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker-compose exec migrate migrate -source file://./migrations -database $MIGRATION_DSN down
+docker-compose exec -T migrate migrate -source file://./migrations -database $MIGRATION_DSN down
 
-docker-compose exec migrate migrate -source file://./migrations -database $MIGRATION_TEST_DSN down
+docker-compose exec -T migrate migrate -source file://./migrations -database $MIGRATION_TEST_DSN down
--- migrate_up.sh
+++ migrate_up.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker-compose exec migrate migrate -source file://./migrations -database $MIGRATION_DSN up
+docker-compose exec -T migrate migrate -source file://./migrations -database $MIGRATION_DSN up
 
-docker-compose exec migrate migrate -source file://./migrations -database $MIGRATION_TEST_DSN up
+docker-compose exec -T migrate migrate -source file://./migrations -database $MIGRATION_TEST_DSN up
